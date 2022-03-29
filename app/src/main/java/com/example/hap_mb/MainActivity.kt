5b404957c2260f98afcde9ac719d2cd5aec8034b
unknown <bhargav.kalyan7@gmail.com>
2022-03-29 16:55:25 +0530

    my project

---
+++ app/src/main/java/com/example/hap_mb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hap_mb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
