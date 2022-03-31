6dcdb44faa2f35d46bf3d46156a619a4f3981b1c
bhargavpalla <bhargav.kalyan7@gmail.com>
2022-03-31 14:01:04 +0530

    First

--- app/src/main/java/com/example/hap_mb/MainActivity.kt
+++ app/src/main/java/com/example/hap_mb/MainActivity.kt
@@ -1,11 +1,47 @@
-package com.example.hap_mb
+ package com.example.hap_mb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ro : Button = findViewById(R.id.roll)
+        val di1 : ImageView = findViewById(R.id.img_d1)
+        val di2 : ImageView = findViewById(R.id.img_d2)
+        ro.setOnClickListener() {
+            val rand = Random().nextInt(6)+1
+            val reso1 = when(rand){
+                1 -> R.drawable.dice1
+                2 -> R.drawable.dice2
+                3 -> R.drawable.dice3
+                4 -> R.drawable.dice4
+                5 -> R.drawable.dice5
+                6 -> R.drawable.dice6
+                else -> R.drawable.dice6
+
+            }
+            di1.setImageResource(reso1)
+            val rand1 = Random().nextInt(6)+1
+            val reso2 = when(rand1){
+                1 -> {R.drawable.dice1
+
+                }
+                2 -> R.drawable.dice2
+                3 -> R.drawable.dice3
+                4 -> R.drawable.dice4
+                5 -> R.drawable.dice5
+                6 -> R.drawable.dice6
+                else -> R.drawable.dice6
+
+            }
+            di2.setImageResource(reso2)
+        }
     }
 }
